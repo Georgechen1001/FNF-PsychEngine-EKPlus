1de1e620f49bf9b098da2ed793a9a2beabf6d203
Georgechen1001 <95180702+Georgechen1001@users.noreply.github.com>
2022-08-30 18:39:40 +0800

    Add files via upload

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -34,12 +34,14 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 		title = 'Gameplay Settings';
 		rpcTitle = 'Gameplay Settings Menu'; //for Discord Rich Presence
 
-		var option:Option = new Option('Controller Mode',
-			'Check this if you want to play with\na controller instead of using your Keyboard.',
-			'controllerMode',
+                #if android
+		var option:Option = new Option('Keyboard Mode',
+			'Check this if you want to play with\nyour keyboard instead of using your Keyboard.',
+			'KeyboardMode',
 			'bool',
 			false);
 		addOption(option);
+                #end
 
 		//I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
 		var option:Option = new Option('Downscroll', //Name
